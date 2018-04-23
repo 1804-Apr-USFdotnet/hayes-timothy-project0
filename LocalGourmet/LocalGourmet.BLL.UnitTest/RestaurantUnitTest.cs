09be454a5073b48388913ba2e795a9b6a66182f5
Timothy Hayes <tjhayes011@gmail.com>
2018-04-23 19:36:29 -0400

    Add Restaurants.json file

--- LocalGourmet/LocalGourmet.BLL.UnitTest/RestaurantUnitTest.cs
+++ LocalGourmet/LocalGourmet.BLL.UnitTest/RestaurantUnitTest.cs
@@ -13,15 +13,37 @@ namespace LocalGourmet.BLL.UnitTest
         {
             Restaurant r = new Restaurant();
             r.Name = "Subway";
+            r.Location = "14961 N Florida Ave, Suite 14961, Florida " +
+                "Crossing Shopping Cntr, Tampa, FL 33613, USA";
+            r.Cuisine = "American";
+            r.PhoneNumber = "(813) 963-2670";
+            r.WebAddress = "www.subway.com";
+            r.Type = "Fast Food";
+            r.Hours = "Mon-Sat 8am-10pm, Sun 9am-9pm";
+
             Restaurant r2 = new Restaurant();
-            r2.Name = "Dominos";
+            r2.Name = "Villa Gallace";
+            r2.Location = "109 Gulf Blvd, Indian Rocks Beach, FL 33785, USA";
+            r2.Cuisine = "Italian";
+            r2.PhoneNumber = "(727) 596-0200";
+            r2.WebAddress = "www.villagallace.com";
+            r2.Type = "Fine Restaurant";
+            r2.Hours = "Mon-Sat 5pm-10:30pm, Sun 4pm-10pm";
+
             Restaurant r3 = new Restaurant();
-            r3.Name = "Italianos";
+            r3.Name = "Three Coins Diner";
+            r3.Location = "7410 N Nebraska Ave, Tampa, FL 33604, USA";
+            r3.Cuisine = "American";
+            r3.PhoneNumber = "(813) 239-1256";
+            r3.WebAddress = "threecoinsdiner.net";
+            r3.Type = "Diner";
+            r3.Hours = "Mon-Sun 12am-11:30pm";
+
             List<Restaurant> rl = new List<Restaurant>();
             rl.Add(r);
             rl.Add(r2);
             rl.Add(r3);
-            Assert.AreEqual("x", Serializer.Serialize(rl));
+            Assert.AreNotEqual("x", Serializer.Serialize(rl));
         }
 
         [TestMethod]
