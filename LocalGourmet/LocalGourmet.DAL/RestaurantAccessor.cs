aaade21999b4728f1967c14a40f6612fad8dfa96
Timothy Hayes <tjhayes011@gmail.com>
2018-04-27 16:44:25 -0400

    Add to DAL

---
+++ LocalGourmet/LocalGourmet.DAL/RestaurantAccessor.cs
@@ -0,0 +1,54 @@
+﻿using LocalGourmet.BLL.Models;
+using LocalGourmet.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalGourmet.DAL
+{
+    // CRUD class for Restaurant
+    public class RestaurantAccessor
+    {
+        // CREATE
+        public async Task AddRestaurantAsync(BLL.Models.Restaurant item)
+        {
+            using (var db = new LocalGourmetDBEntities())
+            {
+                db.Restaurants.Add(LibraryToData(item));
+                await db.SaveChangesAsync();
+            }
+        }
+
+        // READ
+        public IEnumerable<BLL.Models.Restaurant> GetRestaurants()
+        {
+            IEnumerable<BLL.Models.Restaurant> result;
+            using (var db = new LocalGourmetDBEntities())
+            {
+                var dataList = db.Restaurants.ToList();
+                result = dataList.Select(x => DataToLibrary(x)).ToList();
+            }
+            return result;
+        }
+
+        public static BLL.Models.Restaurant DataToLibrary(DL.Restaurant dataModel)
+        {
+            var libModel = new BLL.Models.Restaurant()
+            {
+                Name = dataModel.Name
+            };
+            return libModel;
+        }
+
+        public static DL.Restaurant LibraryToData(BLL.Models.Restaurant libModel)
+        {
+            var dataModel = new DL.Restaurant();
+            {
+                dataModel.Name = libModel.Name;
+            };
+            return dataModel;
+        }
+    }
+}
