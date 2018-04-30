acd7e27127c2c022fc38d9b93d7c39894a97e3f0
Timothy Hayes <tjhayes011@gmail.com>
2018-04-30 12:06:26 -0400

    change

--- SQLCodingChallenge.sql
+++ SQLCodingChallenge.sql
@@ -19,7 +19,7 @@ create table Products.Customers
 [ID] int primary key identity(1,1),
 [Firstname] nvarchar(50) not null,
 [Lastname] nvarchar(50) not null,
-[CardNumber] int not null
+[CardNumber] nchar(16) not null
 );
 
 create table Products.Orders
@@ -31,21 +31,35 @@ create table Products.Orders
 
 insert into Products.Products ([Name], [Price])
 values ('Laptop', $300.00), ('Chair', $50.00), 
-       ('Chocolate Bar', $3.00), ('Car', $15000.00), ('iPhone', $200.00);
+       ('Chocolate Bar', $3.00), ('Car', $1500.00), ('iPhone', $200.00);
 
 insert into Products.Customers ([Firstname], [Lastname], [CardNumber])
-values ('Tina', 'Smith', 1234567890123456), ('Tina', 'Johnson', 1111111111111111),
-       ('James', 'Maddison', 2222222222222222), ('Alex', 'Lloyd', 3333333333333333);
+values ('Tina', 'Smith', '1234567890123456'), ('Tina', 'Johnson', '1111111111111111'),
+       ('James', 'Maddison', '2222222222222222'), ('Alex', 'Lloyd', '3333333333333333');
 
 insert into Products.Orders ([ProductID], [CustomerID])
 values (4,1), (3,1), (1,1), (2,1), (1,2), (5,3), (5,1), (4,4), (3,4), (5,4), (2,4);
 
-select Orders.[ID], Orders.[ProductID], Orders.[CustomerID], 
+select Orders.[ID], Products.[Name], 
        Customers.[Firstname], Customers.[Lastname]
-from Products.Orders
+from (Products.Orders
 join Products.Customers
-on Orders.CustomerID = Customers.ID
+on Orders.[CustomerID] = Customers.[ID])
+join Products.Products
+on Orders.[ProductID] = Products.[ID]
 where Customers.[Firstname] = 'Tina' and 
       Customers.[Lastname] = 'Smith';
 
-select
+select sum(iPhones.[Price]) as [SUM_iPhones]
+from 
+(select Products.[ID], Products.[Name], Products.[Price]
+from Products.Products
+where Products.[Name] = 'iPhone') as [iPhones]
+join Products.Orders
+on [iPhones].[ID] = Orders.[ProductID];
+
+update Products.Products
+set [Price] = $250.00
+where [Name] = 'iPhone';
+
+select * from Products.Products;
